c5ee2f63d2820e4320910fcd15f2a4c30e995c26
behzadp1996 <Behzad.afb2012@yahoo.com>
2021-10-27 13:20:29 +0330

    Complete Vidly Before Database - Ready to work with data

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+
+            var customers = GetCutomer();
+
+            return View(customers);
+
+        }
+
+
+        public ActionResult Detail(int id)
+        {
+            var customer = GetCutomer().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            //return Content(customer.Name);
+            return View(customer);
+
+        }
+
+
+        private IEnumerable<Customer> GetCutomer()
+        {
+            return new List<Customer>
+            {
+                new Customer{Id = 1 , Name="John Smith"},
+                new Customer{Id = 2 , Name="Mary Wiliams"}
+
+            };
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,15 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies / Random
+        public ActionResult Random()
+        {
+
+            var movie = new Movie() { Name = "Avengers" };
+            List<Customer> customerList = new List<Customer>
+            {
+                new Customer (){Name="Behzad Parsa" },
+                new Customer(){Name="Tom Hanks"},
+                new Customer(){Name="Tom Cruise"},
+                new Customer(){Name="Emilia Clark"},
+                new Customer(){Name="Daniel Day Lweis"},
+                new Customer(){Name="Michelle Mongahana"}
+
+            };
+
+
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customerList
+            };
+
+
+
+
+
+
+            //return View(movie);
+            return View(viewModel);
+        }
+
+
+
+
         public ActionResult Index()
         {
-            return View();
+            var movies = GetMovies();
+
+
+            return View(movies);
         }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+
+            return new List<Movie>()
+            {
+                new Movie(){Id=1 , Name="Mission Impossible"},
+                new Movie(){Id=2 , Name="Dune"},
+                new Movie(){Id=3 , Name="Batman"}
+            };
+        }
+
     }
 }
