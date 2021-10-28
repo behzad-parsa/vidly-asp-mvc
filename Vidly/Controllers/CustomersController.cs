59296a733e5446645e22ef06f1391f424860d07c
behzadp1996 <Behzad.afb2012@yahoo.com>
2021-10-28 14:14:55 +0330

    Modify CustomerController To Using DAtabase

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -4,16 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
 
-            var customers = GetCutomer();
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
 
             return View(customers);
 
@@ -22,7 +34,7 @@ namespace Vidly.Controllers
 
         public ActionResult Detail(int id)
         {
-            var customer = GetCutomer().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
             {
                 return HttpNotFound();
@@ -33,14 +45,14 @@ namespace Vidly.Controllers
         }
 
 
-        private IEnumerable<Customer> GetCutomer()
-        {
-            return new List<Customer>
-            {
-                new Customer{Id = 1 , Name="John Smith"},
-                new Customer{Id = 2 , Name="Mary Wiliams"}
+        //private IEnumerable<Customer> GetCutomer()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer{Id = 1 , Name="John Smith"},
+        //        new Customer{Id = 2 , Name="Mary Wiliams"}
 
-            };
-        }
+        //    };
+        //}
     }
 }
