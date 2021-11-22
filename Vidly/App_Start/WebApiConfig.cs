bb503ff9805abe30e94815c645ea01c82d05a1de
behzadp1996 <Behzad.afb2012@yahoo.com>
2021-11-22 22:20:29 +0330

    Modify Json Format To Camel Case

--- Vidly/App_Start/WebApiConfig.cs
+++ Vidly/App_Start/WebApiConfig.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-
+using Newtonsoft.Json;
 namespace Vidly
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+            //Converting Json Fromat To camelCase
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
