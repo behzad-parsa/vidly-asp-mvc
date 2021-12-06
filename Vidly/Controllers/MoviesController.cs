5731ad07aef5cbcf725637cbd43acb03e853baf9
behzadp1996 <Behzad.afb2012@yahoo.com>
2021-12-06 23:17:31 +0330

    Implement DataTable on Movie PAge

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -26,10 +26,12 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
-            var movies = _context.Movies.Include(m=>m.Genre).ToList();
+            // var movies = _context.Movies.Include(m=>m.Genre).ToList();
 
 
-            return View(movies);
+            // return View(movies);
+
+            return View();
         }
         
 
