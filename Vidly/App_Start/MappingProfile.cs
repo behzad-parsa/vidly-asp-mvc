ccd3307296ce58eac5b22c6b1042ad7bba64fc38
behzadp1996 <Behzad.afb2012@yahoo.com>
2021-12-21 21:33:19 +0330

    Implemented CreatNewRental Method On the RentalControllerApi

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -20,6 +20,12 @@ namespace Vidly.App_Start
 
             Mapper.CreateMap<MembershipType, MembershipTypeDto>();
             Mapper.CreateMap<Genre, GenreDto>();
+
+
+            Mapper.CreateMap<Rental, NewRentalDto>();
+            Mapper.CreateMap<NewRentalDto, Rental>();
+            
+
         }
 
     }
--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -12,10 +12,64 @@ namespace Vidly.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
-        [HttpPost]
-        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
         {
-            throw new NotImplementedException();
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRentalDto)
+        {
+
+            //Validation
+
+
+            //finding Customer
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == newRentalDto.CustomerId); 
+
+
+            //finding list of movies
+           var movies =  _context.Movies
+                .Where(m => newRentalDto.MovieIds.Contains(m.Id))
+                .ToList();
+
+
+            List<Rental> rentalList = new List<Rental>();
+
+            //foreach movie exist in the movieList  , the rentalList was added with given customer and current DateTimer
+            movies.ForEach(m => rentalList.Add(new Rental()
+            {
+                Customer = customer,
+                Movie = m,
+                DateRented = DateTime.Now
+
+            }));
+
+
+            //sample code - another form of assign
+
+            //rentalList
+            //    .Select(r => {
+            //        r.DateRented = DateTime.Now;
+            //        r.Customer = customer;
+            //        //r.Movie.Id = movies.
+            //        return r;
+            //    }).ToList();
+
+
+
+
+            _context.Renatls.AddRange(rentalList);
+            _context.SaveChanges();
+
+
+
+
+
+            return Created(new Uri(Request.RequestUri + "/" + newRentalDto.CustomerId) , newRentalDto);
+            //throw new NotImplementedException();
         }
     }
 }
